517028a58760b96588bcbfb78846c20a40188cbb
Matheus Tontini <tontini92@gmail.com>
2019-05-24 11:45:44 +0200

    Almost working

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,52 @@
 const score = document.querySelectorAll('.score');
+const cards = document.querySelectorAll('.cards');
+const jshit = document.querySelector('.js-hit');
+let value;
 
+const deck = new Deck();
 document.addEventListener('DOMContentLoaded', () => {
   // Create and shuffle a new deck of cards
-  const deck = new Deck();
+  
   deck.shuffle();
-  console.log(deck);
-  console.log('face-'+deck.cards[0].rank+'-of-'+deck.cards[0].suit);
+  jshit.addEventListener('click', () => {
+    showCard(1);
+    showCard(0);
+  })
+  
 });
 
-const showCard = () => {
-  
+const showCard = (x) => {
+  let i = randint(0,deck.cards.length);
+  let newCard = document.createElement('div');
+  newCard.className = 'card face-'+deck.cards[i].rank+'-of-'+deck.cards[i].suit;
+  cards[x].appendChild(newCard);
+  sum(deck.cards[i].rank, value);
+  value = sum(deck.cards[i].rank, value);
+  score[x].innerHTML = 'Score: ' + value;
+  check(value,x);
+  value = check(value,x);
 }
+
+const sum = (card, value) => {
+  if (value == undefined) {
+    value = 0;
+  }
+  if (card == 'ace') {
+    value += 11;
+  } else if (card == 'jack' || card == 'queen' || card == 'king') {
+    value += 10;
+  } else {
+    value += Number(card);
+  }
+  return value;
+}
+
+const check = (value, x) => {
+  if (value > 21){
+    score[x].innerHTML = 'Score: ' + value + '<br>You lost!';
+    cards[x].innerHTML = '';
+    return value = 0;
+  }
+  return value;
+}
+
