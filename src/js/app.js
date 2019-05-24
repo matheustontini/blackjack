de186bf6593967e2d36f356b5ab666d621b98086
Matheus Tontini <tontini92@gmail.com>
2019-05-24 10:28:04 +0200

    NPM

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
+const score = document.querySelectorAll('.score');
+
 document.addEventListener('DOMContentLoaded', () => {
   // Create and shuffle a new deck of cards
   const deck = new Deck();
   deck.shuffle();
   console.log(deck);
+  console.log('face-'+deck.cards[0].rank+'-of-'+deck.cards[0].suit);
 });
+
+const showCard = () => {
+  
+}
